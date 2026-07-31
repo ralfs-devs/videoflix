ef9afcd401e662c42d4c6c9543234f1bba57878b
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-07-31 17:49:59 +0200

    User Table In Admin

--- user_auth_app/admin.py
+++ user_auth_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from user_auth_app.models import User
+
+
+@admin.register(User)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin configuration for User instances"""
+
+    list_display = ("username", "is_active",
+                    "is_staff", "is_superuser")
